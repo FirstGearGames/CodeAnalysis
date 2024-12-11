8809efeb8e7b6c2731ed2ad639e60f5f0a81c4bf
FirstGearGames <emailpunfish+github@gmail.com>
2024-12-11 14:41:02 -0500

    Backup

---
+++ FirstGearGames.Roslyn.FishNet.SourceGenerator/CodeBuilding/RemoteProcedureCalls/MethodData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using FirstGearGames.Roslyn.Extensions;
+using FirstGearGames.Roslyn.FishNet.CodeBuilding.Serializers;
+using FirstGearGames.Roslyn.FishNet.Constants;
+using FirstGearGames.Roslyn.FishNet.Helpers.Serializing;
+using FirstGearGames.Roslyn.FishNet.Receivers;
+using Microsoft.CodeAnalysis;
+
+namespace FirstGearGames.Roslyn.CodeBuilding.RemoteProcedureCalls
+{
+    public static class MethodDataExtensions
+    {
+        public static bool IsValid(this MethodData? md)
+        {
+            if (md == null) return false;
+            return !string.IsNullOrWhiteSpace(md.MethodName);
+        }
+    }
+
+    public class MethodData
+    {
+        /// <summary>
+        /// Type the serializer is for.
+        /// </summary>
+        public readonly IMethodSymbol MethodSymbol;
+        /// <summary>
+        /// Name of the generated method.
+        /// </summary>
+        public readonly string MethodName;
+        /// <summary>
+        /// Types needed to be serialized within the RPC.
+        /// </summary>
+        public List<SerializableType> SerializableTypes;
+
+        public MethodData() { }
+
+        public MethodData(IMethodSymbol methodSymbol, List<SerializableType> serializableTypes)
+        {
+            MethodSymbol = methodSymbol;
+            MethodName = methodSymbol.Name;
+            SerializableTypes = serializableTypes;
+        }
+    }
+}
