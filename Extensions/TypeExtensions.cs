ff4487b2a023c05a3bbb4c665cbe4d57214bfc42
FIrstGearGames <firstgeargames@gmail.com>
2026-03-28 18:58:11 -0400

    make GetFullName nullable

--- Extensions/TypeExtensions.cs
+++ Extensions/TypeExtensions.cs
@@ -4,28 +4,18 @@ namespace Nucleus.CodeAnalysis.SourceGenerators.Extensions;
 
 public static class TypeExtensions
 {
-    /// <summary>
-    /// Gets the full name of a Type.
-    /// </summary>
-    /// <remarks>The returned string does not include the global alias.</remarks>
-    public static string GetFullName(this Type type)
-    {
-        string? fullName = type?.FullName;
-            
-        if (fullName is null)
-            return string.Empty;
 
-        return fullName;
-    }
-        
     /// <summary>
     /// Gets the full name of a Type while removing generic arguments and brackets.
     /// </summary>
     /// <remarks>The returned string does not include the global alias.</remarks>
-    public static string GetFullNameWithoutGenerics(this Type type)
+    public static string? GetFullNameWithoutGenerics(this Type type)
     {
-        string fullName = type.GetFullName();
-
+        string? fullName = type.FullName;
+        
+        if (fullName is null)
+            return null;
+        
         int genericMarkerIndex = fullName.IndexOf("`", StringComparison.InvariantCultureIgnoreCase);
         if (genericMarkerIndex >= 0)
             return fullName.Substring(0, genericMarkerIndex);
